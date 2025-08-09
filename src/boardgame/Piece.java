bc7eb48626b4217a38e136f517f4f5b4626e1b90
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-08-08 22:36:38 -0300

    Implementing possible moves of Rook

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -15,7 +15,7 @@ public abstract class Piece {
 
     public abstract boolean[][] possibleMoves();
 
-    public boolean possibleMove(Position Position) {
+    public boolean possibleMove(Position position) {
         return possibleMoves()[position.getRow()][position.getColumn()];
     }
 
